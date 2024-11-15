ab3e7e0ea98e6fc18d0e3ae0b329bd5d138072f7
honeykwak <jenilove0517@ajou.ac.kr>
2024-11-16 00:19:04 +0900

    -change code app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "Hello, Fly.io!"
